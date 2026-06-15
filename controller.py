4e7481a0fce1586d3f5460d3f0a4e50301de2a4e
John Carlo Artillagas <artillagas20@gmail.com>
2026-06-15 13:08:24 +0800

    Add files via upload

--- controller.py
+++ controller.py
@@ -1,82 +1,30 @@
-import model
-import view
+from model import StudentModel
 
 
-def add_student():
-    name = input("Enter student name: ")
-    student_id = input("Enter student ID: ")
-    grade = input("Enter grade: ")
+class StudentController:
 
-    for s in model.students:
-        if s["id"] == student_id:
-            view.show_message(
-                "Student ID already exists!\n"
-            )
-            return
+    def __init__(self):
+        self.model = StudentModel()
 
-    student = {
-        "id": student_id,
-        "name": name,
-        "grade": grade
-    }
+    # USERS
+    def register_user(self, u, p):
+        return self.model.register_user(u, p)
 
-    model.students.append(student)
-    model.save_students()
+    def login_user(self, u, p):
+        return self.model.login_user(u, p)
 
-    view.show_message(
-        "Student added successfully!\n"
-    )
+    # STUDENTS
+    def add_student(self, sid, name, grade):
+        return self.model.add_student(sid, name, grade)
 
+    def get_students(self):
+        return self.model.get_students()
 
-def view_students():
-    view.display_students(model.students)
+    def update_student(self, sid, name, grade):
+        return self.model.update_student(sid, name, grade)
 
+    def delete_student(self, sid):
+        return self.model.delete_student(sid)
 
-def update_student():
-    sid = input("Enter student ID to update: ")
-
-    for s in model.students:
-        if s["id"] == sid:
-            s["name"] = input(
-                "Enter new name: "
-            )
-            s["grade"] = input(
-                "Enter new grade: "
-            )
-
-            model.save_students()
-
-            view.show_message(
-                "Student updated successfully!\n"
-            )
-            return
-
-    view.show_message("Student not found.\n")
-
-
-def delete_student():
-    sid = input("Enter student ID to delete: ")
-
-    for s in model.students:
-        if s["id"] == sid:
-            model.students.remove(s)
-
-            model.save_students()
-
-            view.show_message(
-                "Student deleted successfully!\n"
-            )
-            return
-
-    view.show_message("Student not found.\n")
-
-
-def search_student():
-    sid = input("Enter student ID to search: ")
-
-    for s in model.students:
-        if s["id"] == sid:
-            view.display_student(s)
-            return
-
-    view.show_message("Student not found.\n")
+    def search_student(self, sid):
+        return self.model.search_student(sid)
