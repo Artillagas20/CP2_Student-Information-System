15c923140181600606c256fcfd0b8cad8eed0557
John Carlo Artillagas <artillagas20@gmail.com>
2026-06-15 13:08:55 +0800

    Add files via upload

---
+++ theme.py
@@ -0,0 +1,23 @@
+class Theme:
+
+    DARK = {
+        "bg": "#1e1e2f",
+        "card": "#2b2b3d",
+        "text": "white",
+        "primary": "#3498db",
+        "success": "#2ecc71",
+        "danger": "#e74c3c",
+        "sidebar": "#111827",
+        "active": "#374151"
+    }
+
+    LIGHT = {
+        "bg": "#ecf0f1",
+        "card": "white",
+        "text": "black",
+        "primary": "#3498db",
+        "success": "#2ecc71",
+        "danger": "#e74c3c",
+        "sidebar": "#2c3e50",
+        "active": "#34495e"
+    }
